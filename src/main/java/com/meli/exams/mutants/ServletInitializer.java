3c9490165503f913910647b066567e7c515f808d
fhadida <fhadida@gmail.com>
2017-12-31 20:33:43 -0300

    Adaptación para correr en Google App Engine

---
+++ src/main/java/com/meli/exams/mutants/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.meli.exams.mutants;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(App.class);
+  }
+
+}
