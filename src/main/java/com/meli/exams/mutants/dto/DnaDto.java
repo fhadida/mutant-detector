5731f10f1f97e2c26c962adced0fa69f471b6ca7
Fede <fhadida@gmail.com>
2017-12-31 03:03:12 -0300

    Nivel 2: API REST

---
+++ src/main/java/com/meli/exams/mutants/dto/DnaDto.java
@@ -0,0 +1,26 @@
+package com.meli.exams.mutants.dto;
+
+public class DnaDto {
+	
+	private String[] dna;
+	
+	
+	public DnaDto() {
+		super();
+	}
+
+	public DnaDto(String[] dna) {
+		super();
+		this.dna = dna;
+	}
+
+	public String[] getDna() {
+		return dna;
+	}
+
+	public void setDna(String[] dna) {
+		this.dna = dna;
+	}
+	
+
+}
