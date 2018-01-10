e73a122a760080c0fdff950142fc68f5a54b8fa9
Fede <fh@qbkconsulting.com>
2018-01-09 22:48:45 -0300

    Cambio Gson por Jackson para convertir String[

--- src/main/java/com/meli/exams/mutants/dao/impl/DatastoreDnaDao.java
+++ src/main/java/com/meli/exams/mutants/dao/impl/DatastoreDnaDao.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.repackaged.com.google.gson.Gson;
+
 import com.meli.exams.mutants.dao.DnaDao;
 import com.meli.exams.mutants.model.Dna;
 
@@ -25,8 +27,7 @@ public class DatastoreDnaDao implements DnaDao {
 	private static final String DNA_DATASTORE_KIND = "Dna";
 
 	private DatastoreService datastore;
-//	private KeyFactory dnaKeyFactory;
-	
+
 	
 	@Autowired
 	public void setDatastore(DatastoreService datastore) {
@@ -37,31 +38,25 @@ public class DatastoreDnaDao implements DnaDao {
 		return datastore;
 	}
 	
-//	public KeyFactory getDnaKeyFactory() {
-//		return dnaKeyFactory;
-//	}
-//
-//	@PostConstruct
-//	public void initializeKeyFactories() {
-//		LOG.info("Initializing key factories...");
-//		dnaKeyFactory = getDatastore().newKeyFactory().setKind(DNA_DATASTORE_KIND);
-//	}
-	
 	@Override
 	public Long save(Dna dna) {
-		Entity dnaEntity = createDnaEntity(dna);
-		Key key = getDatastore().put(dnaEntity);
-		LOG.info(String.format("DNA %d saved successfully", dna.hashCode()));
-		LOG.info("DNA Already exists!");
-
+		Key key = null;
+		try {
+			Entity dnaEntity = createDnaEntity(dna);
+			key = getDatastore().put(dnaEntity);
+			LOG.info(String.format("DNA %d saved successfully", dna.hashCode()));
+		} catch (JsonProcessingException e) {
+			LOG.error("No fue posible guardar el ADN: " + e.getLocalizedMessage(), e);
+		}
+		
 		if (key == null) return null;
 		else return key.getId();
 	}
 	
-	private Entity createDnaEntity(Dna dna) {
-		Gson gson = new Gson();
+	private Entity createDnaEntity(Dna dna) throws JsonProcessingException {
 		Entity e = new Entity(DNA_DATASTORE_KIND, dna.hashCode());
-		e.setProperty(RAWDNA_FIELD, gson.toJson(dna.getRawDna()));
+		ObjectMapper mapper = new ObjectMapper();
+		e.setProperty(RAWDNA_FIELD, mapper.writeValueAsString(dna.getRawDna()));
 		e.setProperty(ISMUTANT_FIELD, dna.isMutant());
 		return e;
 	}
